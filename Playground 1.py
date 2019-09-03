a6aab7fd3d448753b845ec35033ec94bce647afa
Joel Nilsson <joel.nilsson9@learnet.se>
2019-09-03 10:08:14 +0200

    första 80 minuterna gjorda på lektionen

--- Playground 1.py
+++ Playground 1.py
@@ -1,1 +1,8 @@
-print("Hejsvejs hörni!")
+print("Hejsvejs allihopa, gissa vem som är här")
+FirstName = "Joel"
+lastName = "Nilsson"
+dateOfBirth = 20011230
+age = 17
+teacher = False
+print(FirstName + " " + lastName ,str(age) , str(teacher))
+print(dateOfBirth)
